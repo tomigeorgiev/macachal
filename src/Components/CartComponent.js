6fab29c34422a8b797142075216cb678e6a0e528
tomigeorgiev <tomigeorgiev83@gmail.com>
2024-03-18 15:03:18 +0200

    Remomve unnecessary div in CartComponent

--- src/Components/CartComponent.js
+++ src/Components/CartComponent.js
@@ -2,15 +2,13 @@ import React from "react";
 
 const CartComponent = () => {
   return (
-    <div>
-      <div className="bot" id="basket">
-        <a href="/cart">
-          <i className="fa-solid fa-basket-shopping"></i>
-          {localStorage.getItem("addedToCart") === "added" && (
-            <p className="num"></p>
-          )}
-        </a>
-      </div>
+    <div className="bot" id="basket">
+      <a href="/cart">
+        <i className="fa-solid fa-basket-shopping"></i>
+        {localStorage.getItem("addedToCart") === "added" && (
+          <p className="num"></p>
+        )}
+      </a>
     </div>
   );
 };
